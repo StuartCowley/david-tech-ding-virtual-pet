b92f05965829898892a09195d9abe1df1462508c
David Ding <dingtechxing16@gmail.com>
2023-03-12 17:14:10 +0000

    Added age and hunger property

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -9,4 +9,28 @@ describe('constructor', () => {
       const pet = new Pet('Fido');
       expect(pet.name).toEqual('Fido');
     });
+
+    it('has an initial age of 0', () => {
+      const pet = new Pet('Fido');
+      expect(pet.age).toEqual(0);
+    });
+
+    it('increases the age by 1', () => {
+      const pet = new Pet('Fido');
+
+      pet.growUp();
+      expect(pet.age).toEqual(1);
+    })
+
+    it('initial hunger level is 0', () => {
+      const pet = new Pet('Fido');
+      expect(pet.hunger).toEqual(0);
+    })
+
+    it('increases the hunger level by 5', () => {
+      const pet = new Pet('Fido');
+
+      pet.growUp();
+      expect(pet.hunger).toEqual(5);
+    })
   });
--- src/pet.js
+++ src/pet.js
@@ -1,5 +1,14 @@
 function Pet(name) {
     this.name = name;
+    this.age = 0;
+    this.hunger = 0;
 }
+
+Pet.prototype = {
+    growUp() {
+        this.age += 1;
+        this.hunger += 5;
+    }
+};
 
 module.exports = Pet;
